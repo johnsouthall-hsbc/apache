8df3d9aeb6846518e3b317553ca27c71668372a4
johnsouthall-hsbc <johnsouthall@hsbc.com>
2017-01-17 13:44:16 +0000

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,10 @@
-class apache:params {
+class apache::params {
 	case $::osfamily {
 	 'Debian': {
 	  $package_name = 'httpd'
 	  $service_name = 'httpd'
 	 }
-	 'RedHat, 'Amazon': {
+	 'RedHat', 'Amazon': {
 	  $package_name = 'httpd'
 	  $service_name = 'httpd'
 	 }
